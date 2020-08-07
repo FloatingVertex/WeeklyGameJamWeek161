7bdd0953abca19df108da1efb5c3d64e0d1e5e97
Pushpinder Singh <pushpindersingh135@gmail.com>
2020-08-07 16:00:38 -0400

    Added online scoreboard backend

---
+++ Assets/Scores.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+
+public static class Scores
+{
+    public static List<KeyValuePair<string, int>> GetScoresAsync(int count = 10)
+    {
+        const string URL = "https://firestore.googleapis.com/v1beta1/projects/wgjweek161/databases/(default)/documents:runQuery";
+        string queryJson = "{\"structuredQuery\": {\"from\": [{\"collectionId\": \"scores\",\"allDescendants\": false}],\"orderBy\":{\"field\":{\"fieldPath\":\"Score\"}},\"limit\": 10},}";
+
+        HttpClient httpClient = new HttpClient();
+        httpClient.BaseAddress = new Uri(URL);
+        httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        var content = new StringContent(queryJson, Encoding.UTF8, "application/json");
+
+        HttpResponseMessage response = httpClient.PostAsync(URL, content).Result;
+
+        var json = SimpleJSON.JSON.Parse( response.Content.ReadAsStringAsync().Result );
+        var scores = new List<KeyValuePair<string, int>>();
+        for(int i = 0; i < json.Count; i++)
+        {
+            int score = json[i]["document"]["fields"]["Score"]["integerValue"].AsInt;
+            string name = json[i]["document"]["fields"]["Name"]["stringValue"];
+            scores.Add(new KeyValuePair<string, int>(name, score));
+        }
+
+        return scores;
+    }
+
+    public static void UploadScores(string username, int score)
+    {
+        const string URL = "https://firestore.googleapis.com/v1beta1/projects/wgjweek161/databases/(default)/documents/scores/";
+        string queryJson = "{\"fields\": {\"Score\": {\"integerValue\": \"" + score + "\"},\"Name\": {\"stringValue\": \"" + username + "\"}}}";
+        HttpClient httpClient = new HttpClient();
+        httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        var content = new StringContent(queryJson, Encoding.UTF8, "application/json");
+
+        httpClient.PostAsync(URL, content);
+    }
+}
