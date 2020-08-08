c8d491b7d8f794fa945c3de60654f41cba65663f
alinutz21 <69359101+alinutz21@users.noreply.github.com>
2020-08-08 22:12:52 +0300

    Added Player Movement and Camera Script

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    //READ THIS
+      // PLEASE ADD THIS SCRIPT TO THE CAMERA IN THE SCENE
+    //READ THIS
+    public Transform player;
+    public Vector3 offset;
+
+    private void Update()
+    {
+        transform.position = new Vector3(player.position.x + offset.x, player.position.y + offset.y, offset.z);
+    }
+}
