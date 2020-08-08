7d2ab17a2a64da6f7e86e91d67168df2196758ec
Pushpinder Singh <pushpindersingh135@gmail.com>
2020-08-08 16:21:23 -0400

    Created Railgun script

--- Assets/Scripts/Bullets/RailGun.cs
+++ Assets/Scripts/Bullets/RailGun.cs
@@ -4,8 +4,36 @@ using UnityEngine;
 
 public class RailGun : MonoBehaviour, IWeapon
 {
+    public GameObject bulletPrefab;
+    public float secondsBetweenShots = 2f;
+    public float bulletSpeed = 100f;
+
+    private bool firing = false;
+    private float deltaToFireAgain = 0f;
+
     public void SetFiring(bool firing)
     {
-        throw new System.NotImplementedException();
+        this.firing = firing;
+    }
+
+    public void FixedUpdate()
+    {
+        deltaToFireAgain -= Time.fixedDeltaTime;
+        if (firing)
+        {
+            if (deltaToFireAgain <= 0.0f)
+            {
+                deltaToFireAgain += secondsBetweenShots;
+                Fire();
+            }
+        }
+
+        deltaToFireAgain = Mathf.Max(deltaToFireAgain, 0.0f);
+    }
+
+    private void Fire()
+    {
+        var bullet = Instantiate(bulletPrefab, transform.position, transform.rotation).GetComponent<Bullet>();
+        bullet.speed = bulletSpeed;
     }
 }
