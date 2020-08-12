adc6f0a447c1db55e6422b6fc5bc8f3e1d1cc7d8
Pushpinder Singh <pushpindersingh135@gmail.com>
2020-08-12 17:26:20 -0400

    Added in game healthbars

---
+++ Assets/Scripts/UI/DestructableHealthBar.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DestructableHealthBar : MonoBehaviour
+{
+    public bool showWhenFull = true;
+
+    public Slider healthBar;
+
+    private Destructable totrack;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        totrack = GetComponentInParent<Destructable>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        healthBar.value = totrack.health / totrack.GetStartingHealth();
+        if (!showWhenFull)
+        {
+            if (totrack.health == totrack.GetStartingHealth())
+            {
+                healthBar.gameObject.SetActive(false);
+            }
+            else
+            {
+                healthBar.gameObject.SetActive(true);
+            }
+        }
+    }
+}
